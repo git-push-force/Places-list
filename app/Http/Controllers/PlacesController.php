51adef0387e34f23e5fcfa4216482e922ae42b29
Anatoly Tayursky <serviceworker@yandex.ru>
2020-06-06 22:17:52 +0300

    Try catch added for controller

--- app/Http/Controllers/PlacesController.php
+++ app/Http/Controllers/PlacesController.php
@@ -13,15 +13,18 @@ class PlacesController extends Controller
     }
 
     public function create(Request $req) {
-        $place = Place::create();
+        try {
+            $place = Place::create();
 
-        $place->name = $req->name;
-        $place->longitude = $req->longitude;
-        $place->latitude = $req->latitude;
-        $place->population = $req->population;
+            $place->name = $req->name;
+            $place->longitude = $req->longitude;
+            $place->latitude = $req->latitude;
+            $place->population = $req->population;
 
-        $place->save();
-
-        return $place;
+            $place->save();
+            return $place;
+        } catch (\Exception $e) {
+            return 'Internal error';
+        }
     }
 }
